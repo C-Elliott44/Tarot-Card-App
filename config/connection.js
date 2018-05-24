176d72b80283ce56ddc52847e8d50fc0323ea5ee
Guillermo Gahbler <mexamericano@gmail.com>
2018-05-24 10:23:49 -0700

    added sequelize to dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize("tarot", "root", "root", {
-  host: "localhost",
+
+var sequelize = new Sequelize(process.env.SEQUELIZE_DB, process.env.SEQUELIZE_USER, process.env.SEQUELIZE_PASSWORD, {
+  host: process.env.SEQUELIZE_HOST,
   dialect: "mysql",
   pool: {
     max: 5,
