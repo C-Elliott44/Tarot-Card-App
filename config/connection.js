f198793cbd422f680c2e3c5428ea9d59457b6a1e
Guillermo Gahbler <mexamericano@gmail.com>
2018-05-30 12:04:01 -0700

    updated env

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,11 @@
 var Sequelize = require("sequelize");
 
+if(process.env.JAWSDB_URL){
+
+  const sequelize = new Sequelize
+  ('mysql://ecq64f8aj9prm5a8:oyeyaya4bwjolqbt@k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/q9smvk3vrgj68skg');
+
+} else {
 
 var sequelize = new Sequelize(process.env.SEQUELIZE_DB, process.env.SEQUELIZE_USER, process.env.SEQUELIZE_PASSWORD, {
   host: process.env.SEQUELIZE_HOST,
@@ -10,5 +16,5 @@ var sequelize = new Sequelize(process.env.SEQUELIZE_DB, process.env.SEQUELIZE_US
     idle: 10000
   }
 });
-
+}
 module.exports = sequelize;
