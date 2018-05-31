d167f73c37f5f66a91b50aed787fd46260b5d855
Guillermo Gahbler <mexamericano@gmail.com>
2018-05-31 10:53:05 -0700

    testing heroku db

--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,10 @@ if (process.env.JAWSDB_URL){
 }
 
 else { 
-var sequelize = new Sequelize(process.env.SEQUELIZE_DB, process.env.SEQUELIZE_USER, process.env.SEQUELIZE_PASSWORD, {
+var sequelize = new Sequelize(process.env.SEQUELIZE_DB, process.env.SEQUELIZE_USER, process.env.SEQUELIZE_PASSWORD
+  , {
   host: process.env.SEQUELIZE_HOST,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
+  dialect: "mysql" 
 })
 }
 
